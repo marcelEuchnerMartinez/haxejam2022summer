332eb022882d028cacdafb34505ca03749aa8b94
Euchner-Martinez Marcel <88530062+marcelEuchnerMartinez@users.noreply.github.com>
2022-07-24 09:20:42 +0200

    Possible basic main structure of game entities and levels.

---
+++ Level.hx
@@ -0,0 +1,17 @@
+
+//import gameobjects.*;
+
+import hxd.Res;
+
+class Level extends h2d.Scene {
+
+    public var walls   : Array< { bounds:h2d.col.Bounds } > = [];
+
+    // contained GameObjects
+    //public var player  : Player;
+    //public var enemies : Array< Enemy > = [];
+
+    public function new() {
+        super();
+    }
+}
